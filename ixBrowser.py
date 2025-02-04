766885d30a1ba81b5671c70e698000921c9717f0
battlekicker <egor.m.pavlov@gmail.com>
2025-02-04 20:23:25 +0300

    2

--- ixBrowser.py
+++ ixBrowser.py
@@ -1,6 +1,4 @@
-import io
-import random
-import time
+import asyncio
 
 from ixbrowser_local_api import IXBrowserClient
 from loguru import logger
@@ -10,42 +8,50 @@ from playwright.async_api import async_playwright, expect
 PROFILE_ID = 1
 
 
-async def start(address: str):
-    with async_playwright() as p:
+async def start():
+    async with async_playwright() as p:
         try:
-            browser = p.chromium.connect_over_cdp(f"http://{debugging_address}")
-            # page = browser.contexts[0].pages[0] if browser.contexts else browser.contexts[0].new_page()
+            context = await p.chromium.launch_persistent_context(
+                '',
+                headless=False,
+                # slow_mo=600
+            )
+
+            page = await context.new_page()
 
             logger.info("Заходим на сайт...")
             await page.goto("https://stackoverflow.com/")
+            await page.wait_for_load_state()
 
             logger.success("Страница загружена!")
 
-            await asyncio.sleep(999)
-
-            accept_cookie_btn = mm_page.get_by_test_id('recovery-phrase-reveal')
+            accept_cookie_btn = page.locator("//button[@id='onetrust-accept-btn-handler']")
             await expect(accept_cookie_btn).to_be_attached()
             await accept_cookie_btn.click()
 
-            await asyncio.sleep(10)
 
+
+            #await asyncio.sleep(999)
+            await page.close()
+            await context.close()
         except Exception as e:
             logger.exception(f"Ошибка Playwright: {e}")
-        finally:
-            browser.close()
 
 
 c = IXBrowserClient()
 data = c.get_profile_list()
+# c.clear_profile_cache_and_cookies(PROFILE_ID)
 if data is None:
-    print('Get profile list error:')
-    print('Error code=', c.code)
-    print('Error message=', c.message)
+    logger.error('Get profile list error. \n'
+                 f'Error code={c.code}'
+                 f'Error message={c.message}')
 else:
     open_result = c.open_profile(PROFILE_ID, cookies_backup=False, load_profile_info_page=False)
     if open_result:
-        debugging_address = open_result["debugging_address"]
-        asyncio.run(start(debugging_address))
+
+        asyncio.run(start())
+        cookie = c.get_profile_cookie(PROFILE_ID)
+        print(cookie)
         c.close_profile(PROFILE_ID)
     else:
         logger.critical("Ошибка открытия профиля")
