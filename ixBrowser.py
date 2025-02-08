3c5108f4b7ffcf355b24b6304a0924ca9fb50010
battlekicker <egor.m.pavlov@gmail.com>
2025-02-08 20:30:47 +0300

    menu added

--- ixBrowser.py
+++ ixBrowser.py
@@ -1,12 +1,8 @@
 import asyncio
 import json
-import pprint
-import os.path
 
-from ixbrowser_local_api import IXBrowserClient
 from loguru import logger
 from playwright.async_api import async_playwright, expect
-
 
 SITE_ADDRESS = "https://stackoverflow.com/"
 PROFILE_ID = 1
@@ -14,49 +10,55 @@ PROFILE2_ID = 2
 FILE = 'data.txt'
 
 
-async def start(ws_endpoint: str):
-    async with async_playwright() as p:
-        try:
-            # context = await p.chromium.launch_persistent_context(
-            #     '',
-            #     headless=False,
-            #     # slow_mo=600
-            # )
+async def start(client, profile_id):
+    open_result = client.open_profile(profile_id, cookies_backup=False, load_profile_info_page=False)
+    if open_result:
+        ws_endpoint = open_result['ws']
+        if ws_endpoint:
+            logger.info(f'Profile ID: {profile_id} | Opened. WS Endpoint: {ws_endpoint}')
+            async with async_playwright() as p:
+                try:
+                    browser = await p.chromium.connect_over_cdp(ws_endpoint)  # Подключаемся к ixBrowser через CDP
+                    context = browser.contexts[0]
 
-            browser = await p.chromium.connect_over_cdp(ws_endpoint)  # Подключаемся к ixBrowser через CDP
-            context = browser.contexts[0]
+                    if context.pages:
+                        page = context.pages[0]
+                    else:
+                        page = await context.new_page()
 
-            if context.pages:
-                page = context.pages[0]
-            else:
-                page = await context.new_page()
+                    logger.info(f"{SITE_ADDRESS} | Entering the site.")
+                    await page.goto(SITE_ADDRESS)
+                    await page.wait_for_load_state()
 
-            logger.info(f"{SITE_ADDRESS} | Entering the site.")
-            await page.goto(SITE_ADDRESS)
-            await page.wait_for_load_state()
+                    logger.success(f"{SITE_ADDRESS} | Page successfully loaded!")
 
-            logger.success(f"{SITE_ADDRESS} | Page successfully loaded!")
+                    try:
+                        accept_cookie_btn = page.locator("//button[@id='onetrust-accept-btn-handler']")
+                        await expect(accept_cookie_btn).to_be_attached()
+                        await accept_cookie_btn.click()
+                    except:
+                        pass
 
-            try:
-                accept_cookie_btn = page.locator("//button[@id='onetrust-accept-btn-handler']")
-                await expect(accept_cookie_btn).to_be_attached()
-                await accept_cookie_btn.click()
-            except:
-                pass
+                    users_btn = page.locator("//a[@id='nav-users']")
+                    await expect(users_btn).to_be_attached()
+                    await users_btn.click()
 
-            users_btn = page.locator("//a[@id='nav-users']")
-            await expect(users_btn).to_be_attached()
-            await users_btn.click()
+                    await asyncio.sleep(2)
+                    await page.close()
+                    await context.close()
+                except Exception as e:
+                    logger.exception(f"Playwright error | {e}")
+        else:
+            logger.critical("Error getting WebSocket endpoint")
+        logger.info(f'Profile ID: {profile_id} | Cookie: \n'
+                    f'{client.get_profile_cookie(profile_id)}')
+        client.close_profile(profile_id)
+    else:
+        logger.critical("Error opening profile")
 
-            await asyncio.sleep(2)
-            await page.close()
-            await context.close()
-        except Exception as e:
-            logger.exception(f"Playwright error | {e}")
 
-
-async def save_cookie(client, file):
-    cookies = client.get_profile_cookie(PROFILE_ID)
+async def save_cookie(client, profile_id, file):
+    cookies = client.get_profile_cookie(profile_id)
     cookies = json.loads(cookies)
     with open(file, 'w') as outfile:
         for cookie in cookies:
@@ -76,8 +78,7 @@ async def save_cookie(client, file):
     #             outfile.write(cookie_str)
     #             outfile.write(',')
 
-    logger.info(f'Cookie saved in file "{file}".')
-    #pprint.pp(cookie)
+    logger.info(f'Profile ID: {profile_id} | Cookie saved in file "{file}".')
 
 
 async def get_cookie(file):
@@ -86,47 +87,22 @@ async def get_cookie(file):
     return cookie
 
 
-c = IXBrowserClient()
-
-data = c.get_profile_list()
-# c.clear_profile_cache_and_cookies(PROFILE_ID)
-if data is None:
-    logger.error('Get profile list error. \n'
-                 f'Error code={c.code}'
-                 f'Error message={c.message}')
-else:
-
-    # Работа с первым профилем
-    open_result = c.open_profile(PROFILE_ID, cookies_backup=False, load_profile_info_page=False)
-    print(open_result)
-    if open_result:
-        ws_endpoint = open_result['ws']
-        if ws_endpoint:
-            logger.info(f'Profile ID: {PROFILE_ID} | Opened. WS Endpoint: {ws_endpoint}')
-            asyncio.run(start(ws_endpoint))
-        else:
-            logger.critical("Error getting WebSocket endpoint")
-        logger.info(f'Profile ID: {PROFILE_ID} | Cookie: \n'
-                    f'{c.get_profile_cookie(PROFILE_ID)}')
-        asyncio.run(save_cookie(c, FILE))
-        c.close_profile(PROFILE_ID)
-    else:
-        logger.critical("Error opening profile")
+async def print_cookie(file):
+    with open(file, 'r') as f:
+        for line in f:
+            print(line)
 
 
-    # Работа со вторым профилем
-    #c.clear_profile_cache_and_cookies(PROFILE2_ID)
-    logger.info(f'Profile ID: {PROFILE2_ID} | Cookie: \n'
-                f'{c.get_profile_cookie(PROFILE2_ID)}')
-    cookie = asyncio.run(get_cookie(FILE))
+async def update_profile_cookie(client, profile_id, file):
+    logger.info(f'Profile ID: {profile_id} | Cookie: \n'
+                f'{client.get_profile_cookie(PROFILE2_ID)}')
+    cookie = await get_cookie(file)
     cookie = str(cookie).replace("['", "[").replace("']", "]").replace("\\n', '", " ")
     cookie = cookie.replace(',\\n', '')
     if cookie != '':
-        c.update_profile_cookie(PROFILE2_ID, cookie)
-        logger.info(f'Profile ID: {PROFILE2_ID} | Cookie updated.')
-        logger.info(f'Profile ID: {PROFILE2_ID} | Cookie: \n'
-                    f'{c.get_profile_cookie(PROFILE2_ID)}')
+        print(client.update_profile_cookie(profile_id, cookie))
+        logger.info(f'Profile ID: {profile_id} | Cookie updated.')
+        logger.info(f'Profile ID: {profile_id} | Cookie: \n'
+                    f'{client.get_profile_cookie(profile_id)}')
     else:
         logger.error('Cookie file is empty!')
-
-
---
+++ menu.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from ixbrowser_local_api import IXBrowserClient
+from loguru import logger
+
+from ixBrowser import start, save_cookie, print_cookie, update_profile_cookie
+
+print('___________________________\n'
+      '1. Нагулять куки\n'
+      '2. Сохранить cookie в файл\n'
+      '3. Вывести содержимое файла с cookie\n'
+      '4. Заменить cookie профиля содержимым файла\n'
+      '5. Завершить работу\n'
+      '___________________________\n')
+
+
+
+
+c = IXBrowserClient()
+data = c.get_profile_list()
+# c.clear_profile_cache_and_cookies(PROFILE_ID)
+if data is None:
+    logger.error('Get profile list error. \n'
+                 f'Error code={c.code}'
+                 f'Error message={c.message}')
+else:
+    while True:
+        choice = int(input('Выберите номер пункта: '))
+        print('___________________________\n')
+
+        if choice == 1:
+            asyncio.run(start(client=c, profile_id=1))
+        elif choice == 2:
+            asyncio.run(save_cookie(client=c, profile_id=1, file="data.txt"))
+        elif choice == 3:
+            asyncio.run(print_cookie(file="data.txt"))
+        elif choice == 4:
+            asyncio.run(update_profile_cookie(client=c, profile_id=2, file="data.txt"))
+        elif choice == 5:
+            print('Завершение работы программы')
+            break
